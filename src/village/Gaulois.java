d1f5dbc9f0ee39d1aba87d35e96a6c58fcadc106
DeltaForce306 <deltaforce3806@gmail.com>
2025-09-30 16:17:10 +0200

    Ajout de la classe Village

--- src/village/Gaulois.java
+++ src/village/Gaulois.java
@@ -4,6 +4,7 @@ public class Gaulois {
 
 	private String nom;
 	private int force;
+	private Village village = null;
 
 	public Gaulois(String nom, int force) {
 		this.force = force;
@@ -14,6 +15,10 @@ public class Gaulois {
 		return nom;
 	}
 
+	public void setVillage(Village village) {
+		this.village = village;
+	}
+
 	public void parler(String texte) {
 		System.out.println(prendreParole() + "\"" + texte + "\"");
 	}
@@ -21,4 +26,24 @@ public class Gaulois {
 	private String prendreParole() {
 		return "Le Gaulois " + nom + " : ";
 	}
+
+	public void sePresenter() {
+		if (this.village == null) {
+			System.out.println("Bonjour, je m'appelle " + this.getNom() + "Je voyage de villages en villages");
+		} else if (this.village.getChef() == this) {
+			System.out.println("Bonjour, je m'appelle " + this.getNom() + ". Je suis le chef " + this.village.getNom());
+		} else {
+			System.out.println("Bonjour, je m'appelle " + this.getNom() + ". J'habite le village " + this.village.getNom());
+		}
+	}
+
+	public void frapper(Romain romain) {
+
+	}
+
+	@Override
+	public String toString() {
+		return nom;
+	}
+
 }
